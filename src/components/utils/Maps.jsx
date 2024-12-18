36b795e3b5c17c0ca89f915b8e87add6119afc9b
stefanny <stefanny@witech.co.id>
2024-12-18 11:44:48 +0700

    [UPD] marker

--- src/components/utils/Maps.jsx
+++ src/components/utils/Maps.jsx
@@ -1,6 +1,17 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'; 
 
 function Map({ coordinate, name }) {
+    var icon = L.icon({
+        iconUrl: '/marker-icon.png',
+        iconRetinaUrl: '/marker-icon-2x.png',
+        shadowUrl: '/marker-shadow.png',
+        iconSize: [25, 41],
+		iconAnchor: [12, 41],
+		popupAnchor: [1, -34],
+		tooltipAnchor: [16, -28],
+		shadowSize: [41, 41]
+    });
+
     return (
         <>
             {coordinate && coordinate.length === 2 ? (
@@ -13,7 +24,7 @@ function Map({ coordinate, name }) {
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     />
-                        <Marker position={coordinate}>
+                        <Marker position={coordinate} icon={icon}>
                         <Popup>{name}</Popup>
                     </Marker>
                 </MapContainer>
